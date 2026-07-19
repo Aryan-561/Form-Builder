21daa0184c6af1ad02f068c66b337a594feed41e
Aryan-561 <aryanmail561@gmail.com>
2026-07-19 20:20:38 +0530

    feat: write createForm service

---
+++ packages/services/form/index.ts
@@ -0,0 +1,64 @@
+import db from "@repo/database";
+import type { CreateFormInput } from "@repo/validators/src/createFrom.schema";
+import type { CreateFieldInput } from "@repo/validators/src/createField.schema";
+import { formsTable } from "@repo/database/models/form"
+import { formFieldsTable } from "@repo/database/models/form_field"
+export class FormService {
+
+    private normalizeFields(fields: CreateFieldInput[]) {
+        return fields.map((field, index) => ({
+            ...field,
+            index,
+            placeholder: field.placeholder?.trim() || null,
+            options:
+                field.type === "select" || field.type === "checkbox"
+                    ? [...new Set(field.options?.map((option) => option.trim()).filter(Boolean) ?? [])]
+                    : null,
+
+        }));
+    }
+
+    async createForm(userId: string, input: CreateFormInput) {
+
+        const normalizedFields = this.normalizeFields(input.fields);
+
+        return db.transaction(async (tx) => {
+            const [form] = await tx.insert(formsTable).values({
+                title: input.title,
+                description: input.description?.trim(),
+                slug: input.slug,
+                status: input.status,
+                createdBy: userId,
+                accessCode: input.status === "private" ? input.accessCode : null
+            }).returning()
+
+            if (!form) {
+                throw new Error("Unable to create form.");
+            }
+
+            if (normalizedFields.length) {
+                await tx.insert(formFieldsTable).values(
+                    normalizedFields.map((field) => ({
+                        formId: form.id,
+                        type: field.type,
+                        label: field.label,
+                        placeholder: field.placeholder,
+                        required: field.required,
+                        options: field.options,
+                        index: field.index
+                    })),
+                );
+            }
+
+            return form
+
+        })
+
+
+
+    }
+
+
+}
+
+
