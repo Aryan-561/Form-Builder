a2d785429a907fc7e8b7d5f1cc3289881a3f8939
Aryan-561 <aryanmail561@gmail.com>
2026-07-23 02:24:10 +0530

    feat: add start and end column

--- packages/database/models/form.ts
+++ packages/database/models/form.ts
@@ -1,15 +1,19 @@
 import { pgEnum, pgTable, uuid, varchar, check, timestamp } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 import { usersTable } from "./user";
-export const formStatus = pgEnum("form_status",["draft", "publish", "private", "unpublish"])
+export const formStatus = pgEnum("form_status", ["draft", "publish", "private", "unpublish"]);
 
-export const formsTable =  pgTable("forms",{
+export const formsTable = pgTable(
+  "forms",
+  {
     id: uuid("id").defaultRandom().primaryKey(),
-    title: varchar("title", {length:55}).notNull(),
-    description: varchar("description", {length: 150}),
-    slug: varchar("slug", {length:180}).notNull(),
+    title: varchar("title", { length: 55 }).notNull(),
+    description: varchar("description", { length: 150 }),
+    slug: varchar("slug", { length: 180 }).notNull(),
     status: formStatus("status").default("draft").notNull(),
-    accessCode: varchar("accessCode", {length:18}),
+    accessCode: varchar("accessCode", { length: 18 }),
+    start: timestamp("start", { withTimezone: true }),
+    stop: timestamp("stop", { withTimezone: true }),
     createdBy: uuid("createdBy")
       .notNull()
       .references(() => usersTable.id, { onDelete: "cascade" }),
@@ -18,15 +22,14 @@ export const formsTable =  pgTable("forms",{
       .defaultNow()
       .notNull()
       .$onUpdate(() => new Date()),
-},
-    (table)=>[
-        check(
-    "access_code_required_when_private",
-    sql`${table.status} <> 'private' OR ${table.accessCode} IS NOT NULL`
-  ),
-    ]
-
-)
+  },
+  (table) => [
+    check(
+      "access_code_required_when_private",
+      sql`${table.status} <> 'private' OR ${table.accessCode} IS NOT NULL`,
+    ),
+  ],
+);
 
 export type SelectForm = typeof formsTable.$inferSelect;
 export type InsertForm = typeof formsTable.$inferInsert;
