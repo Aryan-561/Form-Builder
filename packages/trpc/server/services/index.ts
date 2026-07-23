8d8015befc309614443e6db1de72b1d6f552eddb
Aryan-561 <aryanmail561@gmail.com>
2026-07-24 01:48:56 +0530

    feat: implement backend analytics service and tRPC routes for dashboard statistics

--- packages/trpc/server/services/index.ts
+++ packages/trpc/server/services/index.ts
@@ -1,7 +1,9 @@
 import { UserService } from "@repo/services/user";
 import { FormService } from "@repo/services/form";
 import { FormSubmissionService } from "@repo/services/formSubmission";
+import { AnalyticsService } from "@repo/services/analytics";
 
 export const userService = new UserService();
 export const formService = new FormService();
 export const formSubmissionService = new FormSubmissionService();
+export const analyticsService = new AnalyticsService();
